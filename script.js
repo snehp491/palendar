33a46bb9246f8e5d48731c8ebe34b68d5f65bcec
Sneh Patel <snehp491@gmail.com>
2022-04-03 16:02:16 -0400

    update time while user is on the page

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // create current date time
-var currentDayElement = document.getElementById('currentDay');
+setTime();
 
-var now = moment();
-const date = now.format('dddd, MMMM Do YYYY');
-const time = now.format('h:mm a');
-
-currentDayElement.innerText = time + ' on ' +  date;
+// continue to update date time while user is on page
+setInterval(() => {
+    setTime();
+}, 1000);
 
 // create time slots
 
@@ -15,4 +14,16 @@ var currentHour = new Date().getHours() % 12;
 for  (var i = 0; i < 9; i++) {
     const timeSlot = document.createElement('p');
     timeSlotsElement.appendChild(timeSlot);
+}
+
+
+function setTime() {
+    var currentDayElement = document.getElementById('currentDay');
+
+    var now = moment();
+    const date = now.format('dddd, MMMM Do YYYY');
+    const time = now.format('h:mm a');
+
+    currentDayElement.innerText = time + ' on ' +  date;
+
 }
