67f7ceb917105d12657bb6cfc95ab26c4d399c1c
Sneh Patel <snehp491@gmail.com>
2022-04-03 15:56:40 -0400

    create time slot elements by appending

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+// create current date time
 var currentDayElement = document.getElementById('currentDay');
 
 var now = moment();
 const date = now.format('dddd, MMMM Do YYYY');
-const time = now.format('HH:mm:ss a');
+const time = now.format('h:mm a');
 
 currentDayElement.innerText = time + ' on ' +  date;
+
+// create time slots
+
+var timeSlotsElement = document.getElementById('timeSlots');
+var currentHour = new Date().getHours() % 12;
+
+for  (var i = 0; i < 9; i++) {
+    const timeSlot = document.createElement('p');
+    timeSlotsElement.appendChild(timeSlot);
+}
